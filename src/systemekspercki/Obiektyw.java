98f3e8b96c0ca398fb2350ce2d374b873e73b145
maikhel <mlecicki@gmail.com>
2016-01-18 17:36:07 +0100

    first commit

---
+++ src/systemekspercki/Obiektyw.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package systemekspercki;
+
+/**
+ *
+ * @author Majk
+ */
+public class Obiektyw {
+    private String producent;
+    private double cena;
+    
+}
---
+++ src/systemekspercki/Pytanie.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package systemekspercki;
+
+import java.util.ArrayList;
+/**
+ *
+ * @author Majk
+ */
+public class Pytanie {
+    private int ileOdpowiedzi;
+    private int numer;
+    private Pytanie nastepnePytanie;
+    private ArrayList mozliweOdpowiedzi;
+    private Odpowiedz odpowiedz;
+    
+    
+    
+    public void show(){
+        //wyswietla pytanie
+    }
+    
+    public void odpowiedz(){
+        
+    }
+}
